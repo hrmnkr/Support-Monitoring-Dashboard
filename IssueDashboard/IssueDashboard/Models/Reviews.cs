21a659d9f40f7ca7325ee40fe69f2865e1f82f1a
Harman Kaur <hrmnkr76@gmail.com>
2026-03-30 01:22:13 +0530

    Added Review Dashboard

---
+++ IssueDashboard/IssueDashboard/Models/Reviews.cs
@@ -0,0 +1,17 @@
+﻿namespace IssueDashboard.Models
+{
+    public class Review
+    {
+        public int SrNo { get; set; }
+        public DateTime ReviewDate { get; set; }
+        public string DeviceName { get; set; }
+        public string UserName { get; set; }
+        public string ReviewText { get; set; }
+        public DateTime? ReplyDate { get; set; }
+        public string ReplyText { get; set; }
+        public string Platform { get; set; } // Android / iOS
+
+        // Derived
+        public bool IsReplied => !string.IsNullOrWhiteSpace(ReplyText);
+    }
+}
---
+++ IssueDashboard/IssueDashboard/Providers/Reviews/IReviewProvider.cs
@@ -0,0 +1,9 @@
+﻿using IssueDashboard.Models;
+
+namespace IssueDashboard.Providers.Reviews
+{
+    public interface IReviewProvider
+    {
+        List<Review> GetReviews();
+    }
+}
